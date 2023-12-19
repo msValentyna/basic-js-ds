b17bd4c941139876d7d8ef9b5e22af7a58390cdf
Valentyna Ustimenko <>
2023-12-19 22:32:00 +0200

    done queue task

--- src/queue.js
+++ src/queue.js
@@ -13,24 +13,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
 
+  constructor() {
+    this.head = null;
+    this.length = 0;
+  }
+
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    return this.head;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue(value) {
+    if (this.length === 0) {
+      this.head = new Node(value);
+    } else {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = new Node(value);
+    }
+    this.length ++;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.length === 0) {
+      return null;
+    }
+
+    const current = this.head.value;
+    this.head = this.head.next;
+    this.length--;
+
+    return current;
   }
 }
+
+// const queue = new Queue;
+// queue.enqueue(1);
+// queue.enqueue(2);
+// queue.enqueue(3);
+// queue.dequeue();
+
+// queue.getUnderlyingList();
+
+// console.log(queue);
 
 module.exports = {
   Queue
 };
+
+// node src/queue.js
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,105 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
+
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.next = null;
+//   }
+// }
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.length = 0;
+//   }
+
+//   addElement(value) {
+//     if (this.length === 0) {
+//       this.head = new Node(value);
+//     } else {
+//       let current = this.head;
+//       // console.log('current ', current)
+
+//       while (current.next) {
+//         current = current.next;
+//         // console.log('current next ', current)
+//       }
+
+//       current.next = new Node(value);
+//       // console.log('cn', current.next);
+//     }
+
+//     this.length++;
+//   }
+// }
+
 function removeKFromList(/* l, k */) {
+  // function findAndRemoveElement(l, k) {
+  //   let current = l.head;
+  //   let prev = null;
+  //   let indexK = 0;
+
+  //   while(current) {
+  //     if (current.value === k) {
+  //       if (prev === null) {
+  //         l.head = current.next;
+  //       } else {
+  //         prev.next = current.next;
+  //       }
+  //       l.length--;
+  //       return indexK;
+  //     }
+  //     prev = current;
+  //     current = current.next;
+  //     indexK++;
+  //   }
+  //   // l.length--;
+  //   return -1;
+  // }
+
+  // let index;
+  // while ((index = findAndRemoveElement(l, k)) !== -1) {
+  //   findAndRemoveElement(l, k);
+  // }
+  // return l;
+
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const list = new LinkedList;
+// list.addElement(3);
+// list.addElement(1);
+// list.addElement(2);
+// list.addElement(3);
+// list.addElement(4);
+// list.addElement(5);
+
+// const found = 3
+
+// console.log(list);
+// removeKFromList(list, found);
+// console.log(list);
+
 module.exports = {
   removeKFromList
 };
+
+// node src/remove-from-list.js
